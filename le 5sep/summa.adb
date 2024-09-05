79b67016b43439369420758ff6c4135ce6a76d55
Joel Wejdenstål <jwejdenstal@icloud.com>
2024-09-05 14:51:43 +0200

    use for loop

--- le 5sep/summa.adb
+++ le 5sep/summa.adb
@@ -4,12 +4,10 @@ use Ada.Text_IO;
 procedure Summa is
     Sum : Integer := 0;
     Input : Integer := 0;
-    Amount : Integer := 5;
 begin
-    while Amount /= 0 loop
+    for I in 0..5 loop
         Input := Integer'Value(Get_Line);
         Sum := Sum + Input;
-        Amount := Amount - 1;
     end loop;
 
     Put_Line("Summan är: " & Integer'Image(Sum));
