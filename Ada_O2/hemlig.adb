4cc556455582f83bcefceccf7044829ef185eb96
Joel Wejdenstål <jwejdenstal@icloud.com>
2024-09-12 15:08:29 +0200

    reset to proper state

--- Ada_O2/hemlig.adb
+++ Ada_O2/hemlig.adb
@@ -1,8 +1,6 @@
 with Ada.Text_IO;
-with Ada.Integer_Text_IO;
 with Ada.Float_Text_IO;
 use Ada.Text_IO;
-use Ada.Integer_Text_IO;
 use Ada.Float_Text_IO;
 
 procedure Hemlig is
@@ -12,32 +10,51 @@ procedure Hemlig is
     Step : Float;
     VATPercentage : Float;
 begin
-    --Put("Första pris: ");
-    --Get(FirstPrice);
-    --Skip_Line;
---
-    --Put("Sista pris: ");
-    --Get(LastPrice);
-    --Skip_Line;
---
-    --Put("Steg: ");
-    --Get(Step);
-    --Skip_Line;
---
-    --Put("Momsprocent: ");
-    --Get(VATPercentage);
-    --Skip_Line;
+    loop 
+        Put("Första pris: ");
+        Get(FirstPrice);
+        Skip_Line;
 
-    FirstPrice := 7.5;
-    LastPrice := 12.0;
-    Step := 0.5;
-    VATPercentage := 10.0;
+        if FirstPrice < 0.0 then
+            Put_Line("Felaktigt värde!");
+        else
+            exit;
+        end if;
+    end loop;
+
+    loop 
+        Put("Sista pris: ");
+        Get(LastPrice);
+        Skip_Line;
+
+        if LastPrice < FirstPrice then
+            Put_Line("Felaktigt värde!");
+        else
+            exit;
+        end if;
+    end loop;
+
+    loop 
+        Put("Steg: ");
+        Get(Step);
+        Skip_Line;
+
+        if Step < 0.0 then
+            Put_Line("Felaktigt värde!");
+        else
+            exit;
+        end if;
+    end loop;
+
+    Put("Momsprocent: ");
+    Get(VATPercentage);
+    Skip_Line;
 
     New_Line;
     Put_Line("============ Momstabell ============");
     Put_Line("Pris utan moms  Moms   Pris med moms");
     CurrentPrice := FirstPrice;
-    while CurrentPrice <= LastPrice loop
+    while CurrentPrice <= LastPrice+0.01 loop
         Put(CurrentPrice, Fore => 6, Aft => 2, Exp => 0);
         Put(CurrentPrice * 0.01 * VATPercentage, Fore => 8, Aft => 2, Exp => 0);
         Put(CurrentPrice + CurrentPrice * 0.01 * VATPercentage, Fore => 9, Aft => 2, Exp => 0);
