e2f9527a5290a343fa7d15dc3b74fc351d941cac
Joel Wejdenstål <jwejdenstal@icloud.com>
2024-09-26 06:29:24 +0200

    Ada.O3.1: typedefs

--- Ada.O3.1/recordmanipulation.adb
+++ Ada.O3.1/recordmanipulation.adb
@@ -7,6 +7,37 @@ with Ada.Numerics;                      use Ada.Numerics;
 with Ada.Numerics.Elementary_Functions; use Ada.Numerics.Elementary_Functions;
 
 procedure RecordManipulation is
+    type DS1 is record
+        W : Character;
+        P : String(1..80);
+    end record;
+
+    type DS2_U is record
+        S : Float;
+        Z : String(1..80);
+    end record;
+
+    type DS2 is record
+        D : DS2_U;
+        U : DS2_U;
+    end record;
+
+    type DS3_U1 is record
+        Y : Character;
+        Q : Character;
+    end record;
+
+    type DS3_U2 is record
+        T : Boolean;
+        L : Character;
+    end record;
+
+    type DS3 is record
+        J : DS3_U1;
+        B : DS3_U1;
+        O : DS3_U2;
+    end record;
+
     procedure GetOne is
     begin
         Put_Line("Hello, world!");
