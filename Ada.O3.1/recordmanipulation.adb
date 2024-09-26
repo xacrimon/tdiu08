376247857789dc015d08b0843b814a0f47aa0d6c
Joel Wejdenstål <jwejdenstal@icloud.com>
2024-09-26 06:22:56 +0200

    Ada.O3.1: skeleton

---
+++ Ada.O3.1/recordmanipulation.adb
@@ -0,0 +1,41 @@
+-- matol699: Samarbetat med joewe275, Joel Wejdenstål, samma program
+
+with Ada.Text_IO;                       use Ada.Text_IO;
+with Ada.Integer_Text_IO;               use Ada.Integer_Text_IO;
+with Ada.Float_Text_IO;                 use Ada.Float_Text_IO;
+with Ada.Numerics;                      use Ada.Numerics;
+with Ada.Numerics.Elementary_Functions; use Ada.Numerics.Elementary_Functions;
+
+procedure RecordManipulation is
+    procedure GetOne is
+    begin
+        Put_Line("Hello, world!");
+    end GetOne;
+
+    procedure PutOne is
+    begin
+        Put_Line("Hello, world!");
+    end PutOne;
+
+    procedure GetTwo is
+    begin
+        Put_Line("Hello, world!");
+    end GetTwo;
+
+    procedure PutTwo is
+    begin
+        Put_Line("Hello, world!");
+    end PutTwo;
+
+    procedure GetThree is
+    begin
+        Put_Line("Hello, world!");
+    end GetThree;
+
+    procedure PutThree is
+    begin
+        Put_Line("Hello, world!");
+    end PutThree;
+begin
+    Put_Line("Hello, world!");
+end RecordManipulation;
