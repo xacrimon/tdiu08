7f6a5e47bbf1f69b251f1a4bddeff3d3a355586f
Joel Wejdenstål <jwejdenstal@icloud.com>
2024-09-26 06:30:06 +0200

    comment

--- Ada.O3.1/recordmanipulation.adb
+++ Ada.O3.1/recordmanipulation.adb
@@ -1,4 +1,4 @@
--- matol699: Samarbetat med joewe275, Joel Wejdenstål, samma program
+-- joewe275: Samarbetat med matol699, Mattis Olevall, samma program
 
 with Ada.Text_IO;                       use Ada.Text_IO;
 with Ada.Integer_Text_IO;               use Ada.Integer_Text_IO;
