e45ed6a690842c2aedcf8f22245eb0f4473eff30
matol699 <matol699@tlvm-4-4-2.ad.liu.se>
2024-09-26 09:16:18 +0200

    Format and variable names

--- Ada.O3.1/record_manipulation.adb
+++ Ada.O3.1/record_manipulation.adb
@@ -1,86 +1,87 @@
 -- joewe275: Samarbetat med matol699, Mattis Olevall, samma program
 
-with Ada.Text_IO;                       use Ada.Text_IO;
-with Ada.Float_Text_IO;                 use Ada.Float_Text_IO;
+with Ada.Text_IO;       use Ada.Text_IO;
+with Ada.Float_Text_IO; use Ada.Float_Text_IO;
 
 procedure Record_Manipulation is
-    GetPreprint : String := "Mata in datamängd: ";
-    PutPreprint : String := "Inmatad datamängd: ";
+    Get_Preprint : String := "Mata in datamängd: ";
+    Put_Preprint : String := "Inmatad datamängd: ";
 
     procedure Eat_Separator is
-        NoSpaceFound : exception;
+        No_Space_Found : exception;
         Scratch : Character;
     begin
-        Get(Scratch);
+        Get (Scratch);
         if Scratch /= ' ' then
-            raise NoSpaceFound;
+            raise No_Space_Found;
         end if;
     end Eat_Separator;
 
     procedure Write_Separator is
     begin
-        Put(" ");
+        Put (" ");
     end Write_Separator;
 
-    procedure Get(Data : out Boolean) is
-        InvalidBoolean : exception;
+    procedure Get (Data : out Boolean) is
+        Invalid_Boolean : exception;
         Scratch : Character;
     begin
-        Get(Scratch);
+        Get (Scratch);
         case Scratch is
-        when 'T' =>
-            Data := True;
-        when 'F' =>
-            Data := False;
-        when others => raise InvalidBoolean;
+            when 'T' =>
+                Data := True;
+            when 'F' =>
+                Data := False;
+            when others =>
+                raise Invalid_Boolean;
         end case;
     end Get;
 
-    procedure Put(Data : in Boolean) is
+    procedure Put (Data : in Boolean) is
     begin
         if Data then
-            Put("True");
+            Put ("True");
         else
-            Put("False");
+            Put ("False");
         end if;
     end Put;
 
     type DS1 is record
         W : Character;
-        P : String(1..4);
+        P : String (1 .. 4);
     end record;
 
-    procedure Get(Data : out DS1) is
+    procedure Get (Data : out DS1) is
     begin
-        Get(Data.W);
+        Get (Data.W);
         Eat_Separator;
-        Get(Data.P);
+        Get (Data.P);
     end Get;
 
-    procedure Put(Data : in DS1) is
+    procedure Put (Data : in DS1) is
     begin
-        Put(Data.W);
+        Put (Data.W);
         Write_Separator;
-        Put(Data.P);
+        Put (Data.P);
     end Put;
 
     type DS2_U is record
         S : Float;
-        Z : String(1..4);
+        Z : String (1 .. 4);
     end record;
 
-    procedure Get(Data : out DS2_U) is
+    procedure Get (Data : out DS2_U) is
     begin
-        Get(Data.S);
+        Get (Data.S);
         Eat_Separator;
-        Get(Data.Z);
+        Get (Data.Z);
     end Get;
 
-    procedure Put(Data : in DS2_U) is
+    procedure Put (Data : in DS2_U) is
     begin
-        Put(Data.S, Fore => 0, Aft => 3, Exp => 0);
+        Put (Data.S, Fore => 0, Aft => 3, Exp => 0);
         Write_Separator;
-        Put(Data.Z);
+        Put (Data.Z);
     end Put;
 
     type DS2 is record
@@ -88,18 +89,18 @@ procedure Record_Manipulation is
         U : DS2_U;
     end record;
 
-    procedure Get(Data : out DS2) is
+    procedure Get (Data : out DS2) is
     begin
-        Get(Data.D);
+        Get (Data.D);
         Eat_Separator;
-        Get(Data.U);
+        Get (Data.U);
     end Get;
 
-    procedure Put(Data : in DS2) is
+    procedure Put (Data : in DS2) is
     begin
-        Put(Data.D);
+        Put (Data.D);
         Write_Separator;
-        Put(Data.U);
+        Put (Data.U);
     end Put;
 
     type DS3_U1 is record
@@ -107,18 +108,18 @@ procedure Record_Manipulation is
         Q : Character;
     end record;
 
-    procedure Get(Data : out DS3_U1) is
+    procedure Get (Data : out DS3_U1) is
     begin
-        Get(Data.Y);
+        Get (Data.Y);
         Eat_Separator;
-        Get(Data.Q);
+        Get (Data.Q);
     end Get;
 
-    procedure Put(Data : in DS3_U1) is
+    procedure Put (Data : in DS3_U1) is
     begin
-        Put(Data.Y);
+        Put (Data.Y);
         Write_Separator;
-        Put(Data.Q);
+        Put (Data.Q);
     end Put;
 
     type DS3_U2 is record
@@ -126,18 +127,18 @@ procedure Record_Manipulation is
         L : Character;
     end record;
 
-    procedure Get(Data : out DS3_U2) is
+    procedure Get (Data : out DS3_U2) is
     begin
-        Get(Data.T);
+        Get (Data.T);
         Eat_Separator;
-        Get(Data.L);
+        Get (Data.L);
     end Get;
 
-    procedure Put(Data : in DS3_U2) is
+    procedure Put (Data : in DS3_U2) is
     begin
-        Put(Data.T);
+        Put (Data.T);
         Write_Separator;
-        Put(Data.L);
+        Put (Data.L);
     end Put;
 
     type DS3 is record
@@ -146,52 +147,52 @@ procedure Record_Manipulation is
         O : DS3_U2;
     end record;
 
-    procedure Get(Data : out DS3) is
+    procedure Get (Data : out DS3) is
     begin
-        Get(Data.J);
+        Get (Data.J);
         Eat_Separator;
-        Get(Data.B);
+        Get (Data.B);
         Eat_Separator;
-        Get(Data.O);
+        Get (Data.O);
     end Get;
 
-    procedure Put(Data : in DS3) is
+    procedure Put (Data : in DS3) is
     begin
-        Put(Data.J);
+        Put (Data.J);
         Write_Separator;
-        Put(Data.B);
+        Put (Data.B);
         Write_Separator;
-        Put(Data.O);
+        Put (Data.O);
     end Put;
 
-    Data_One : DS1;
-    Data_Two : DS2;
+    Data_One   : DS1;
+    Data_Two   : DS2;
     Data_Three : DS3;
 begin
-    Put_Line("För DS1:");
-    Put(GetPreprint);
-    Get(Data_One);
+    Put_Line ("För DS1:");
+    Put (Get_Preprint);
+    Get (Data_One);
     Skip_Line;
 
-    Put(PutPreprint);
-    Put(Data_One);
-    New_Line(2);
+    Put (Put_Preprint);
+    Put (Data_One);
+    New_Line (2);
 
-    Put_Line("För DS2:");
-    Put(GetPreprint);
-    Get(Data_Two);
+    Put_Line ("För DS2:");
+    Put (Get_Preprint);
+    Get (Data_Two);
     Skip_Line;
 
-    Put(PutPreprint);
-    Put(Data_Two);
-    New_Line(2);
+    Put (Put_Preprint);
+    Put (Data_Two);
+    New_Line (2);
 
-    Put_Line("För DS3:");
-    Put(GetPreprint);
-    Get(Data_Three);
+    Put_Line ("För DS3:");
+    Put (Get_Preprint);
+    Get (Data_Three);
     Skip_Line;
 
-    Put(PutPreprint);
-    Put(Data_Three);
-    New_Line(1);
+    Put (Put_Preprint);
+    Put (Data_Three);
+    New_Line (1);
 end Record_Manipulation;
