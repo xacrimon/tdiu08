86e7f01d53847e118e0aa54be83458572e8a9b3b
Joel Wejdenstål <jwejdenstal@icloud.com>
2024-10-01 08:39:34 +0200

    p2.0 retry

--- joel/subprograms.adb
+++ joel/subprograms.adb
@@ -3,49 +3,83 @@
 with Ada.Text_IO;                       use Ada.Text_IO;
 with Ada.Integer_Text_IO;               use Ada.Integer_Text_IO;
 with Ada.Float_Text_IO;                 use Ada.Float_Text_IO;
-with Ada.Numerics;                      use Ada.Numerics;
 with Ada.Numerics.Elementary_Functions; use Ada.Numerics.Elementary_Functions;
 
 procedure Subprograms is
-    function ComputeAverage(
-        D1, D2, D3: in Integer
-    ) return Integer is
-        Sum, FloatAvg : Float;
+    -- Deluppgift 1.
+    -- Notering för assistent: Left-hand-side och Right-hand-side av operatorn.
+    function "-" (Lhs : in String;
+                  Rhs : in Integer) return Integer is
     begin
-        Sum := Float(D1) + Float(D2) + Float(D3);
-        FloatAvg := Sum / 3.0;
-        return Integer (Float'Ceiling (FloatAvg));
-    end ComputeAverage;
+        return Integer(Float'Value(Lhs)) - Rhs;
+    end "-";
 
-    function "+" (
-        A : in String;
-        B : in Character
-    ) return Integer is
+    -- Deluppgift 2.
+    procedure ComputeCircleArea(Radius : in Integer;
+                                Area : out Integer) is
+        F_Radius : Float := Float(Radius);
+        F_Area : Float;
     begin
-        return Integer(Float'Value(A) - Float'Value((1 => B)));
-    end "+";
+        F_Area := F_Radius * F_Radius * 3.14;
+        Area := Integer(Float'Floor(F_Area));
+    end ComputeCircleArea;
 
-    procedure ComputeProduct(
-        FactorOne : out Integer;
-        FactorTwo : out Float;
-        Product: out Integer
-    ) is
-        ReadChar : Character;
-        FloatProduct : Float;
+    -- Deluppgift 3.
+    function ComputeEquilateralTriangleArea(Side : in Float) return Float is
+        Raw_Area : Float;
     begin
-        Put("Mata in ett tecken: ");
-        Get(ReadChar);
-        Put("Mata in ett flyttal: ");
-        Get(FactorTwo);
+        Raw_Area := Side * (Side * Sqrt(3.0) / 2.0) / 2.0;
+        return Float'Floor(Raw_Area);
+    end ComputeEquilateralTriangleArea;
 
-        FactorOne := Integer'Value((1 => ReadChar));
-        FloatProduct := Float(FactorOne) * FactorTwo;
-        if FloatProduct > 0.0 then
-            Product := Integer(Float'Ceiling(FloatProduct));
-        else
-            Product := Integer(Float'Floor(FloatProduct));
-        end if;
-    end ComputeProduct;
+    -- Variabler för deluppgift 1.
+    DiffLhs : String(1..3);
+    DiffRhs, DiffResult : Integer;
+
+    -- Variabler för deluppgift 2.
+    Radius : Integer;
+    CircleArea: Integer;
+
+    -- Variabler för deluppgift 3.
+    Side : Float;
+    TriangleArea : Float;
 begin
-    Put_Line("Hello, World!");
+    -- Deluppgift 1 inläsning och beräkning.
+    Put("Mata in en sträng: ");
+    Get(DiffLhs);
+    Put("Mata in ett heltal: ");
+    Get(DiffRhs);
+    Skip_Line;
+    DiffResult := DiffLhs - DiffRhs;
+
+    -- Deluppgift 1 utmatning.
+    Put("Den beräknade differensen mellan ");
+    Put(DiffLhs);
+    Put(" och ");
+    Put(DiffRhs, Width => 0);
+    Put(" blir ");
+    Put(DiffResult, Width => 0);
+    New_Line(2);
+
+    -- Deluppgift 2 inläsning och beräkning.
+    Put("Mata in längden: ");
+    Get(Radius);
+    Skip_Line;
+    ComputeCircleArea(Radius, CircleArea);
+
+    -- Deluppgift 2 utmatning.
+    Put("Den beräknade arean blev: ");
+    Put(CircleArea, Width => 0);
+    New_Line(2);
+
+    -- Deluppgift 3 inläsning och beräkning.
+    Put("Mata in längden: ");
+    Get(Side);
+    Skip_Line;
+    TriangleArea := ComputeEquilateralTriangleArea(Side);
+
+    -- Deluppgift 3 utmatning.
+    Put("Den beräknade arean blev: ");
+    Put(TriangleArea, Fore => 0, Aft => 1, Exp => 0);
+    New_Line;
 end Subprograms;
