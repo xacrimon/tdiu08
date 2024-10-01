cdfc08adb53066ae66d2f6fc153d522223ddd779
Joel Wejdenstål <jwejdenstal@icloud.com>
2024-10-01 08:46:11 +0200

    don't rt float for conv

--- joel/subprograms.adb
+++ joel/subprograms.adb
@@ -11,7 +11,7 @@ procedure Subprograms is
     function "-" (Lhs : in String;
                   Rhs : in Integer) return Integer is
     begin
-        return Integer(Float'Value(Lhs)) - Rhs;
+        return Integer'Value(Lhs) - Rhs;
     end "-";
 
     -- Deluppgift 2.
