5b5d466eb41338e81ab32b610f9e66110b97df61
Joel Wejdenstål <jwejdenstal@icloud.com>
2024-10-08 08:54:35 +0200

    Ada.O4: samarbetskommentarer

--- Ada.O4/test_exceptions.adb
+++ Ada.O4/test_exceptions.adb
@@ -1,3 +1,6 @@
+-- joewe275: Samarbetat med matol699, Mattis Olevall, samma program
+-- matol699: Samarbetat med joewe275, Joel Wejdenstål, samma program
+
 with Ada.Text_IO;         use Ada.Text_IO;
 with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
 with Ada.Strings.Fixed;   use Ada.Strings.Fixed;
