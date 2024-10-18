5e2e405fe5e8c3aa709313c102dd2fa954d5c5d2
matol699 <root@mattis-pc.localdomain>
2024-10-18 11:54:02 +0200

    Remove debug prints

--- build.sh
+++ build.sh
@@ -2,16 +2,13 @@
 
 rm build/$1/$2
 mkdir -p build/$1/obj
-echo $3
 
 if [ $3 -eq 0 ]
 then
-    echo "build"
     gnatmake -D build/$1/obj -o build/$1/$2 $1/$2.adb
     ./build/$1/$2
 elif [ $3 -eq 1 ]
 then
-    echo "valgrind"
     mkdir -p build/$1/debug
     cd build/$1/debug
     gnatmake -D ../obj -g -o ../$2 ../../../$1/$2.adb
