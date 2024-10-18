8f3ff7751bdaa36cf1e449010861c582bbbd082d
matol699 <root@mattis-pc.localdomain>
2024-10-18 12:51:48 +0200

    Remove unnecessary specifications

--- Ada.O5/date.ads
+++ Ada.O5/date.ads
@@ -7,16 +7,6 @@ package Date is
     Month_Error  : exception;
     Day_Error    : exception;
 
-    procedure Get_Correct_String (S : out String);
-
-    function Parse_Date_Field (Field : in String) return Integer;
-    procedure Parse_Date (Item :    out Date_Type; 
-                          S    : in     String);
-
-    function Days_In_Month (Month, Year : in Integer) return Integer;
-    procedure Validate_Date (Item : in Date_Type);
-    function Zero_Pad (Value, Length : in Integer) return String;
-
     procedure Get (Item : out Date_Type);
     procedure Put (Item : in Date_Type);
 
