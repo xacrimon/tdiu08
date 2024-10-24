a5e0f1cc1f57bc097e8ae5cb9cfd8ed199d116e4
Joel Wejdenstål <jwejdenstal@icloud.com>
2024-10-24 22:21:35 +0200

    format

--- Cplusplus.O1.1/terminal_io.cpp
+++ Cplusplus.O1.1/terminal_io.cpp
@@ -4,7 +4,8 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
     int heltal;
     float flyttal;
     char tecken;
@@ -13,7 +14,8 @@ int main() {
     cout << "Skriv in ett heltal: ";
     cin >> heltal;
     cin.ignore(1000, '\n');
-    cout << "Du skrev in heltalet: " << heltal << endl << endl;
+    cout << "Du skrev in heltalet: " << heltal << endl
+         << endl;
 
     cout << "Skriv in fem heltal: ";
     cin >> heltal;
@@ -27,61 +29,72 @@ int main() {
     cout << heltal << " ";
     cin >> heltal;
     cin.ignore(1000, '\n');
-    cout << heltal << endl << endl;
+    cout << heltal << endl
+         << endl;
 
     cout << "Skriv in ett flyttal: ";
     cin >> flyttal;
     cin.ignore(1000, '\n');
-    cout << "Du skrev in flyttalet: " << fixed << setprecision(3) << flyttal << endl << endl;
+    cout << "Du skrev in flyttalet: " << fixed << setprecision(3) << flyttal << endl
+         << endl;
 
     cout << "Skriv in ett heltal och ett flyttal: ";
     cin >> heltal >> flyttal;
     cin.ignore(1000, '\n');
     cout
         << "Du skrev in heltalet:" << setw(10) << heltal << endl
-        << "Du skrev in flyttalet:" << setw(9) << fixed << setprecision(4) << flyttal << endl << endl;
-    
+        << "Du skrev in flyttalet:" << setw(9) << fixed << setprecision(4) << flyttal << endl
+        << endl;
+
     cout << "Skriv in ett flyttal och ett heltal: ";
     cin >> flyttal >> heltal;
     cin.ignore(1000, '\n');
     cout
         << "Du skrev in heltalet:" << setfill('-') << setw(10) << heltal << endl
-        << "Du skrev in flyttalet:" << setfill('-') << setw(9) << fixed << setprecision(4) << flyttal << endl << endl;
+        << "Du skrev in flyttalet:" << setfill('-') << setw(9) << fixed << setprecision(4) << flyttal << endl
+        << endl;
 
     cout << "Skriv in ett tecken: ";
     cin >> tecken;
     cin.ignore(1000, '\n');
-    cout << "Du skrev in tecknet: " << tecken << endl << endl;
+    cout << "Du skrev in tecknet: " << tecken << endl
+         << endl;
 
     cout << "Skriv in ett ord: ";
     cin >> text;
     cin.ignore(1000, '\n');
-    cout << "Du skrev in ordet: " << text << endl << endl;
+    cout << "Du skrev in ordet: " << text << endl
+         << endl;
 
     cout << "Skriv in ett heltal och ett ord: ";
     cin >> heltal >> text;
     cin.ignore(1000, '\n');
     cout
         << "Du skrev in heltalet |" << heltal << "|"
-        << " och ordet |" << text << "|." << endl << endl;
+        << " och ordet |" << text << "|." << endl
+        << endl;
 
     cout << "Skriv in ett tecken och ett ord: ";
     cin >> tecken >> text;
     cin.ignore(1000, '\n');
-    cout << "Du skrev in \"" << text << "\" och \"" << tecken << "\"." << endl << endl;
+    cout << "Du skrev in \"" << text << "\" och \"" << tecken << "\"." << endl
+         << endl;
 
     cout << "Skriv in ett ord och ett tecken: ";
     cin >> text >> tecken;
     cin.ignore(1000, '\n');
-    cout << "Du skrev in \"" << text << "\" och \"" << tecken << "\"." << endl << endl;
+    cout << "Du skrev in \"" << text << "\" och \"" << tecken << "\"." << endl
+         << endl;
 
     cout << "Skriv in en textrad: ";
     getline(cin, text);
-    cout << "Du skrev in: \"" << text << "\"." << endl << endl;
+    cout << "Du skrev in: \"" << text << "\"." << endl
+         << endl;
 
     cout << "Skriv in en till rad text: ";
     getline(cin, text);
-    cout << "Du skrev in: \"" << text << "\"." << endl << endl;
+    cout << "Du skrev in: \"" << text << "\"." << endl
+         << endl;
 
     cout << "Skriv in en rad med tre ord: ";
     cin >> text;
@@ -91,5 +104,6 @@ int main() {
     cout << "\"" << text << "\" och ";
     cin >> text;
     cin.ignore(1000, '\n');
-    cout << "\"" << text << "\"." << endl << endl;
+    cout << "\"" << text << "\"." << endl
+         << endl;
 }
