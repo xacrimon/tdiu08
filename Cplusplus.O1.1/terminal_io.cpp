4d1691c87142b82f63a3ff1bed2aea0b84a2be78
Joel Wejdenstål <jwejdenstal@icloud.com>
2024-10-24 22:03:48 +0200

    Cplusplus.O1.1

---
+++ Cplusplus.O1.1/terminal_io.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <iomanip>
+#include <limits>
+
+using namespace std;
+
+void skip_line() {
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+}
+
+int main() {
+    int heltal;
+    float flyttal;
+    char tecken;
+    string text;
+
+    cout << "Skriv in ett heltal: ";
+    cin >> heltal;
+    skip_line();
+    cout << "Du skrev in heltalet: " << heltal << endl << endl;
+
+    cout << "Skriv in fem heltal: ";
+    cin >> heltal;
+    cout << "Du skrev in heltalen: ";
+    cout << heltal << " ";
+    cin >> heltal;
+    cout << heltal << " ";
+    cin >> heltal;
+    cout << heltal << " ";
+    cin >> heltal;
+    cout << heltal << " ";
+    cin >> heltal;
+    skip_line();
+    cout << heltal << endl << endl;
+
+    cout << "Skriv in ett flyttal: ";
+    cin >> flyttal;
+    skip_line();
+    cout << "Du skrev in flyttalet: " << fixed << setprecision(3) << flyttal << endl << endl;
+
+    cout << "Skriv in ett heltal och ett flyttal: ";
+    cin >> heltal >> flyttal;
+    skip_line();
+    cout
+        << "Du skrev in heltalet:" << setw(10) << heltal << endl
+        << "Du skrev in flyttalet:" << setw(9) << fixed << setprecision(4) << flyttal << endl << endl;
+    
+    cout << "Skriv in ett flyttal och ett heltal: ";
+    cin >> flyttal >> heltal;
+    skip_line();
+    cout
+        << "Du skrev in heltalet:" << setfill('-') << setw(10) << heltal << endl
+        << "Du skrev in flyttalet:" << setfill('-') << setw(9) << fixed << setprecision(4) << flyttal << endl << endl;
+
+    cout << "Skriv in ett tecken: ";
+    cin >> tecken;
+    skip_line();
+    cout << "Du skrev in tecknet: " << tecken << endl << endl;
+
+    cout << "Skriv in ett ord: ";
+    cin >> text;
+    skip_line();
+    cout << "Du skrev in ordet: " << text << endl << endl;
+
+    cout << "Skriv in ett heltal och ett ord: ";
+    cin >> heltal >> text;
+    skip_line();
+    cout
+        << "Du skrev in heltalet |" << heltal << "|"
+        << " och ordet |" << text << "|." << endl << endl;
+
+    cout << "Skriv in ett tecken och ett ord: ";
+    cin >> tecken >> text;
+    skip_line();
+    cout << "Du skrev in \"" << text << "\" och \"" << tecken << "\"." << endl << endl;
+
+    cout << "Skriv in ett ord och ett tecken: ";
+    cin >> text >> tecken;
+    skip_line();
+    cout << "Du skrev in \"" << text << "\" och \"" << tecken << "\"." << endl << endl;
+
+    cout << "Skriv in en textrad: ";
+    getline(cin, text);
+    cout << "Du skrev in: \"" << text << "\"." << endl << endl;
+
+    cout << "Skriv in en till rad text: ";
+    getline(cin, text);
+    cout << "Du skrev in: \"" << text << "\"." << endl << endl;
+
+    cout << "Skriv in en rad med tre ord: ";
+    cin >> text;
+    cout << "Du skrev in: ";
+    cout << "\"" << text << "\", ";
+    cin >> text;
+    cout << "\"" << text << "\" och ";
+    cin >> text;
+    skip_line();
+    cout << "\"" << text << "\"." << endl << endl;
+}
