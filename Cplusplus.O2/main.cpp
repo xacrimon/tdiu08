673470cff2ea1e2cb65dd4b70fc74c190d8e27d8
Joel Wejdenstål <jwejdenstal@icloud.com>
2024-11-12 02:29:40 +0100

    prevent fallthru

--- Cplusplus.O2/main.cpp
+++ Cplusplus.O2/main.cpp
@@ -103,12 +103,16 @@ static void dispatch(const Subprogram subprogram)
     {
     case FACTORIAL:
         subprogram_factorial();
+        break;
     case MULTIPLY_STRING:
         subprogram_multiply_string();
+        break;
     case SWITCH_PLACES:
         subprogram_switch_places();
+        break;
     case CALCULATE_LENGTH:
         subprogram_calculate_length();
+        break;
     case EXIT:
         break;
     }
