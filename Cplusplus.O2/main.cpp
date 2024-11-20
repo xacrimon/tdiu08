3b251b51dcb96cd71bb0738e9cd75de93d904e94
matol699 <root@mattis-pc.>
2024-11-20 11:33:49 +0100

    Remove static and fix other comments

--- Cplusplus.O2/main.cpp
+++ Cplusplus.O2/main.cpp
@@ -6,64 +6,78 @@
 #include <limits>
 #include <cmath>
 
-static void clear_input()
+const int FACTORIAL = 1;
+const int MULTIPLY_STRING = 2;
+const int SWITCH_PLACES = 3;
+const int CALCULATE_LENGTH = 4;
+const int EXIT = 5;
+
+void clear_input()
 {
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 }
 
-static int op_factorial(const int n)
+int op_factorial(int const n)
 {
     int result{1};
 
     for (int i{1}; i <= n; i++)
+    {
         result *= i;
+    }
 
     return result;
 }
 
-static void subprogram_factorial()
+void subprogram_factorial()
 {
+    int n, result;
+
     std::cout << "Mata in ett heltal: ";
-    int n;
     std::cin >> n;
     clear_input();
-    const int result = op_factorial(n);
+    result = op_factorial(n);
     std::cout << "Fakulteten av " << n << " är " << result << std::endl;
 }
 
-static std::string op_multiply_string(const std::string &text, const int multiplier)
+std::string op_multiply_string(std::string const &text,
+                               int const multiplier)
 {
     std::string result;
 
     for (int i{0}; i < multiplier; i++)
+    {
         result += text;
+    }
 
     return result;
 }
 
-static void subprogram_multiply_string()
+void subprogram_multiply_string()
 {
-    std::cout << "Mata in en text och ett heltal: ";
-    std::string text;
+    std::string text, result;
     int multiplier;
+
+    std::cout << "Mata in en text och ett heltal: ";
     std::cin >> text >> multiplier;
     clear_input();
-    const std::string result = op_multiply_string(text, multiplier);
+    result = op_multiply_string(text, multiplier);
     std::cout << "Den multiplicerade texten är " << result << std::endl;
 }
 
-static void op_switch_places(int &a, float &b)
+void op_switch_places(int &a, float &b)
 {
-    const int tmp{a};
+    int tmp{a};
     a = static_cast<int>(std::ceil(b));
     b = static_cast<float>(tmp);
 }
 
-static void subprogram_switch_places()
+void subprogram_switch_places()
 {
-    std::cout << "Mata in ett heltal och ett flyttal: ";
     int a;
     float b;
+
+    std::cout << "Mata in ett heltal och ett flyttal: ";
     std::cin >> a >> b;
     clear_input();
     op_switch_places(a, b);
@@ -72,33 +86,31 @@ static void subprogram_switch_places()
         << "Flyttalets värde är nu " << std::fixed << std::setprecision(1) << b << std::endl;
 }
 
-static void op_calculate_length(const std::string &text_1, const std::string &text_2, int &total_length, float &average_length)
+void op_calculate_length(std::string const &text_1,
+                         std::string const &text_2,
+                         int &total_length,
+                         float &average_length)
 {
     total_length = text_1.length() + text_2.length();
     average_length = static_cast<float>(total_length) / 2.0f;
 }
 
-static void subprogram_calculate_length()
+void subprogram_calculate_length()
 {
+    int total_length;
+    float average_length;
+
     std::cout << "Mata in två ord: ";
     std::string text_1, text_2;
     std::cin >> text_1 >> text_2;
     clear_input();
-    int total_length;
-    float average_length;
     op_calculate_length(text_1, text_2, total_length, average_length);
     std::cout
         << "Totallängd: " << total_length << "\n"
         << "Medellängd: " << std::fixed << std::setprecision(1) << average_length << std::endl;
 }
 
-const int FACTORIAL = 1;
-const int MULTIPLY_STRING = 2;
-const int SWITCH_PLACES = 3;
-const int CALCULATE_LENGTH = 4;
-const int EXIT = 5;
-
-static void dispatch(const int subprogram)
+void dispatch(int const subprogram)
 {
     switch (subprogram)
     {
@@ -117,10 +129,12 @@ static void dispatch(const int subprogram)
     }
 }
 
-static int menu()
+int menu()
 {
-    for (;;)
+    while (true)
     {
+        int choice;
+
         std::cout
             << "1. Beräkna N-fakultet.\n"
             << "2. Multiplicera en sträng.\n"
@@ -129,7 +143,6 @@ static int menu()
             << "5. Avsluta programmet.\n"
             << "Val: ";
 
-        int choice;
         std::cin >> choice;
         clear_input();
 
@@ -147,9 +160,9 @@ int main()
 {
     std::cout << "Välkommen till huvudmenyn!" << std::endl;
 
-    for (;;)
+    while (true)
     {
-        const int subprogram{menu()};
+        int subprogram{menu()};
 
         if (subprogram == EXIT)
         {
